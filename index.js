08a5f1b33bef595d0bff139daa9750bfbbd36f82
Nisseboy <60318517+Nisseboy@users.noreply.github.com>
2023-05-11 09:01:36 +0200

    added getting over it

--- index.js
+++ index.js
@@ -163,6 +163,7 @@ let gamesMenu = {
 };
 gamesMenu.addons.push(createGameIframe("Subway Surfers", "https://subway-surfer-monaco.nugeshinia.repl.co/"));
 gamesMenu.addons.push(createGameIframe("Time Shooter", "https://timeshooter.application08.repl.co/"));
+gamesMenu.addons.push(createGameIframe("Getting Over It", "https://scratch.mit.edu/projects/389464290/embed/"));
 gamesMenu.addons.push(createGameIframe("Recoil", "https://sipragio06.github.io/recoil/"));
 addons.push(gamesMenu);
 
